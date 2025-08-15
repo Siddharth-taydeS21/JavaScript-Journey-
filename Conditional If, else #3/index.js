b975635cc7a61eb983b9dcaa8092d7a5a8c9b06f
Siddharth Tayde <taydesiddharth999@gmail.com>
2025-08-15 19:25:21 +0530

    initial commit

---
+++ Conditional If, else #3/index.js
@@ -0,0 +1,17 @@
+let score =    84 ;
+
+let = result = score >= 35 ? "Pass" : "Fail";
+console.log(result)
+
+let login = false;
+
+let visibility = login == true ? "is_logedin" : "please_log_in";
+console.log(visibility)
+
+let num = 45 ;
+console.log(num * 3 == 74 ? "Correct" : "Incorrect")
+
+let marks = 84 ;
+
+let grade = marks >= 90 ? "A+" : marks >= 80 ? "A" : "B";
+console.log(grade)
