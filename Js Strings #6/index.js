65d72b9dd4911fbb6595cc47b8f43798926d4b64
Siddharth Tayde <taydesiddharth999@gmail.com>
2025-08-17 20:52:44 +0530

    Strings tutorial folder added

---
+++ Js Strings #6/index.js
@@ -0,0 +1,33 @@
+let a = "Siddharth"
+console.log(a)
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+console.log(a[7]);
+console.log(a[8]);
+ console.log(a.length)
+
+ let real_name = "siddhrath";
+ let friend = "prashik";
+console.log("His name is " + real_name + " and his friend name is " + friend)
+
+//Striing Template Litrels : -
+
+console.log(`His name is ${real_name} and his friend name is ${friend}`)
+
+let b = "Siddharth Tayde"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(3,7))
+console.log(b.slice(4))
+
+let c = "Front End Engineer"
+console.log(c)
+console.log(c.replace("Front End", "Full Stack"))
+
+console.log(b.concat(" ",c))
