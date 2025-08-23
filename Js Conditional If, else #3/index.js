732697a5dd7d3313c0bdb972f7aad3a619528da8
Siddharth Tayde <taydesiddharth999@gmail.com>
2025-08-23 17:05:38 +0530

    Tutorial no. 7 Added

--- Js Conditional If, else #3/index.js
+++ Js Conditional If, else #3/index.js
@@ -1,17 +1,169 @@
-let score =    84 ;
+//exm 1.	
+// Add two numbers.
 
-let = result = score >= 35 ? "Pass" : "Fail";
-console.log(result)
+let a = 5;
+let b = 4;
+let c = a + b;
+console.log(c);
 
-let login = false;
+//exm 2.	
+// Subtract two numbers.
 
-let visibility = login == true ? "is_logedin" : "please_log_in";
-console.log(visibility)
+let d = a - b;
+console.log(d);
 
-let num = 45 ;
-console.log(num * 3 == 74 ? "Correct" : "Incorrect")
+//exm 3.	
+// multiply two numbers.
 
-let marks = 84 ;
+let e = a * b;
+console.log(e);
 
-let grade = marks >= 90 ? "A+" : marks >= 80 ? "A" : "B";
-console.log(grade)
+//exm 4.	
+// devide two numbers.
+
+let f = a / b;
+console.log(f);
+
+//exm 5.	
+// Find remider of duvision.
+
+let reminder = a % b;
+console.log(reminder);
+
+//exm 6.	
+// Find exponentiation (2 ** 3).
+
+let a1 = 2;
+let a2 = 3;
+let a3 = a1 ** a2;
+
+console.log(a3);
+
+//exm 7.	
+// 	Check equality (==).
+
+let a4 = "5";
+let a5 = 5;
+
+let a6 = a4 == a5;
+console.log(a6);
+
+//exm 8.	
+// 	Check equality (===).
+
+let a7 = a4 === a5;
+console.log(a7);
+
+//exm 9.	
+// 	Check equality (!=).
+
+let a8 = a4 != a5;
+console.log(a8);
+
+//exm 10.	
+// 	Check strict equality (!==).
+let a9 = a4 !== a5;
+console.log(a9);
+
+//exm 11.	
+// 	Greater than operator.
+
+let b1 = 5;
+let b2 = 7;
+let b3 = b2 > b1;
+
+console.log(b3);
+
+//exm 12.	
+// 	lesser than operator.
+
+let b4 = b1 < b2;
+console.log(b4);
+
+//exm 13.	
+// 	Greater or equal operator.
+
+let b5 = 8;
+let b6 = b5 >= b2;
+console.log(b6);
+
+//exm 14.	
+// 	lesser or equal operator.
+ 
+let b7 = b2 <= b5;
+console.log(b7);
+
+//exm 15.	
+// 	Use logical AND (&&).
+
+b8 = (b5 >= b2 && b5 >= b3)
+console.log(b8)
+// it gives ture when both or all conditions are true . if both or all conditions are false it gives false. and if only one condition is true or false it gives true or false.
+
+//exm 16.	
+// 	Use logical AND (||).
+
+let b9 = (5 > 9||6 > 4);
+console.log(b9)
+
+//exm 17.	
+// 	Use logical AND (!).
+
+let b10 = !(6 > 4);
+console.log(b10)
+
+//exm 18.	
+// Ternary operator to check even/odd..
+
+let c1 = 52; 
+let EvenorOdd = (c1 % 2 === 0) ? "Odd" : "Even";
+console.log(EvenorOdd);
+
+marks =  95;
+grade = (marks >=  95)? "A+" : (marks >= 90)? "A" : "B";
+console.log(grade);
+
+let Visibility = "IsLogedInn"
+let welcome =(Visibility === "IsLogedInn") ? "Welcome Back" : "Please LogIn";
+console.log(welcome);
+
+let amount = 1000;
+let Discount = (amount > 500) ? "Discount" : "No Discount";
+console.log(Discount);
+
+//exm 20 to 23.	
+// Compound Assignment +=.
+// Compound Assignment -=.
+// Compound Assignment *=.
+// Compound Assignment /=.
+
+let x = 5; let y = 10;
+
+y += x; 
+console.log(y);
+
+y -= x; 
+console.log(y);
+
+y *= x; 
+console.log(y);
+
+y /= x; 
+console.log(y);
+
+//exm 24.	
+// Pre-increment vs post increment.
+
+let d1 =  5;
+let d2 = ++d1;
+console.log(d1)
+console.log(d2)
+
+let d3 = 7;
+let d4 = d3++;
+console.log(d3)
+console.log(d4)
+
+//exm 25.	
+// Pre-Decriment vs post Decriment.
+
---
+++ Js Post,Pree - Increment, Decrement #7/index.js
@@ -0,0 +1,69 @@
+//1. 
+let a = 5;
+console.log(a++);  //prints a first - 5 then it becoms 6.
+console.log(a); // now number is 6.
+
+//2. 
+let b = 7;
+console.log(++b); //at first it will perform increment. then it prints 8.
+console.log(b); //8.
+
+//3. 
+let x = 10;
+let y = x++; // at first x is asigned to y which means y = 10. after that x becoms 11 because of increment.
+console.log(x, y); // prints (11, 10)
+
+//4. 
+let p = 15;
+let q = ++p; //at first it will perform increment, p-> p = 16. then it prints 16.
+console.log(p, q); //prints 16, 16
+
+//5.
+let n = 20;
+console.log(n++ + ++n);  
+console.log(n); // (20 + 22 = 42) output = 20 42. 
+
+//6.
+let c = 3;
+let d = c++ + c++ + ++c; 
+//first step - c = 3 increment going to happen when we use it again.
+//second step - c = 4 increment going to happen when we use it again..
+//third step - first c has incremented value of 5 then becuse of ++c - increment first, 5 becoms 6, ind uses 6. 
+console.log(c, d); // c = 6, d = 13 -> 6, 13.
+
+//7.
+let num = 8;
+console.log(++num + num++ + num);
+// first step - increment first : num = 9, then uses 9.
+// second step - uses 9 first and increment will happen in next step.
+// third step - num = 10 - because of increment sign(++) in peivious step.
+
+console.log(num);// prints 28.
+
+//8.
+let k = 4;
+let result = k++ + ++k + k++ + ++k;
+// first step - uses k = 4, increment will happen in next step. final value = 4. 
+// second step - incremented value of k is now 5 in these step increment first will make it 6. final value = 6.
+// third step - uses k = 6, increment will happen in next step. final value = 6.
+// fourt step - incremented value of k is now 7. in these step increment first will make it 8. final value = 8.
+console.log(result, k);// prints 24, 8.
+
+//9.
+let i = 2;
+let j = i++ + i++ + ++i + ++i;
+// first step - uses 2, increment will happen in next step. final value = 2.
+// second step - incremented value of i is now 3. increment will happen in next step. final value = 3. 
+// third step - incremented value of i is now 4. in these step increment first will make it 5. final value = 5. 
+// fourth step - value of i is now 5. in these step increment first will make it 6. final value = 6. 
+console.log(i, j); // prints 6, 16
+
+//10.
+let m = 5;
+let value = ++m + m++ + ++m + m++ + m;
+// first step - in these step increment first will make m = 6. final value = 6. 
+// second step - uses 6, increment will happen in next step. final value = 6.
+// third step - in these step increment first will make m = 7. final value = 8.
+// fourth step - it uses 7, increment will happen in next step. final value = 8.
+// fourth step - here is the final and incremented value of m is = 9.
+console.log(m, value); // prints 9, 37.
