aecf8b2164479eaf678521adf8593347bb635241
Siddharth Tayde <taydesiddharth999@gmail.com>
2025-08-26 16:09:58 +0530

    loops practice file aded

---
+++ Js Loops #4/practice.js
@@ -0,0 +1,106 @@
+// Loops(30)
+
+// 1.	Print 1–10 using for loop.
+for (let a = 1; a <= 10; a++) {
+    console.log(a)
+}
+
+// 2.	Print even numbers 1–20.
+for (let b = 1; b <= 20; b++) {
+  if(b % 2 === 0) 
+    console.log(b); 
+}
+
+// 3.	Print odd numbers 1–20.
+for (let c = 1; c < 20; c++) {
+    if (c % 2 !== 0)
+        console.log(c);
+}
+
+// // 4.	Print multiplication table of 5.
+for (let d = 1; d < 10; d++) {
+    console.log(d * 5)
+}
+
+// 5.	Print factorial of 5.
+let factorial = 1;
+for (let i = 1; i <= 20; i++) {
+   console.log(factorial = factorial * i)
+}
+
+// 6.	Sum numbers 1–100.
+let sum = 0;
+for (let i = 1; i < 101; i++) {
+    console.log(sum = sum + i) 
+}
+
+// 7.	Print Fibonacci series up to 10 terms.
+let n1 = 0;
+let n2 = 1;
+console.log(n1);
+console.log(n2);
+for (let i = 3; i <= 10; i++) {
+    let next = n1 + n2;
+    console.log(next);
+    n1 = n2;
+    n2 = next;
+}
+
+// 8.	Reverse counting 10–1.
+for (let d = 10; d >= 1; d--) {
+   console.log(d);
+}
+
+// 9.	Print pattern of * (triangle).
+ for (let i = 1; i <= 5; i++) {
+    row = "";
+    for (let star = 1; star <= i; star++) {
+        row = row + "#";
+    }
+    console.log(row)
+ }
+
+// revert this triangle example :
+for (let i = 1; i <= 5; i++) {
+  row = "";
+  for (let j = 1; j <= 5 - i + 1; j++) {
+    row = row + "#"
+  }
+  console.log(row)
+}
+
+// 10.	While loop: 1–10.
+let n = 1
+while (n <= 10) {
+  console.log(n++);
+}
+
+// 11.	While loop: sum of 1–50.
+let sum1 = 0;
+let n3 = 1;
+while(n3 <= 50){
+  sum1 = sum1 + n3;
+  n3++;
+}
+console.log(sum1)
+
+// 12.	While loop: reverse digits of number.
+
+// 13.	Do while loop: print 1–5.
+// 14.	Do while: input until user enters 0.
+// 15.	Nested loops: multiplication table 1–5.
+// 16.	For…of: iterate array.
+// 17.	For…of: iterate string.
+// 18.	For…in: iterate object.
+// 19.	Break statement example.
+// 20.	Continue statement example.
+// 21.	Infinite loop with break condition.
+// 22.	Loop to count vowels in string.
+// 23.	Loop through array and double values.
+// 24.	Loop to find max number in array.
+// 25.	Loop to reverse array.
+// 26.	Loop to count digits in number.
+// 27.	Nested loop for 2D array.
+// 28.	Loop to check prime numbers 1–50.
+// 29.	Loop with flag variable.
+// 30.	Loop to print sum of even numbers.
