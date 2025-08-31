a3a9cb924f52fa680583416c12bf86b347e68790
Siddharth Tayde <taydesiddharth999@gmail.com>
2025-08-31 12:11:30 +0530

    miner change

--- Js Loops #4/practice.js
+++ Js Loops #4/practice.js
@@ -1,93 +1,100 @@
-// Loops(30)
+// // Loops(30)
 
-// 1.	Print 1–10 using for loop.
-for (let a = 1; a <= 10; a++) {
-    console.log(a)
-}
+// // 1.	count 1–10 using for loop.
+// for (let a = 1; a <= 10; a++) {
+//     console.log(a)
+// }
 
-// 2.	Print even numbers 1–20.
-for (let b = 1; b <= 20; b++) {
-  if(b % 2 === 0) 
-    console.log(b); 
-}
+// // 2.	count even numbers 1–20.
+// for (let b = 1; b <= 20; b++) {
+//   if(b % 2 === 0) 
+//     console.log(b); 
+// }
 
-// 3.	Print odd numbers 1–20.
-for (let c = 1; c < 20; c++) {
-    if (c % 2 !== 0)
-        console.log(c);
-}
+// // 3.	count odd numbers 1–20.
+// for (let c = 1; c < 20; c++) {
+//     if (c % 2 !== 0)
+//         console.log(c);
+// }
 
-// // 4.	Print multiplication table of 5.
-for (let d = 1; d < 10; d++) {
-    console.log(d * 5)
-}
+// // // 4.	count multiplication table of 5.
+// for (let d = 1; d < 10; d++) {
+//     console.log(d * 5)
+// }
 
-// 5.	Print factorial of 5.
-let factorial = 1;
-for (let i = 1; i <= 20; i++) {
-   console.log(factorial = factorial * i)
-}
+// // 5.	count factorial of 5.
+// let factorial = 1;
+// for (let i = 1; i <= 20; i++) {
+//    console.log(factorial = factorial * i)
+// }
 
-// 6.	Sum numbers 1–100.
-let sum = 0;
-for (let i = 1; i < 101; i++) {
-    console.log(sum = sum + i) 
-}
+// // 6.	Sum numbers 1–100.
+// let sum = 0;
+// for (let i = 1; i < 101; i++) {
+//     console.log(sum = sum + i) 
+// }
 
-// 7.	Print Fibonacci series up to 10 terms.
-let n1 = 0;
-let n2 = 1;
-console.log(n1);
-console.log(n2);
-for (let i = 3; i <= 10; i++) {
-    let next = n1 + n2;
-    console.log(next);
-    n1 = n2;
-    n2 = next;
-}
+// // 7.	count Fibonacci series up to 10 terms.
+// let n1 = 0;
+// let n2 = 1;
+// console.log(n1);
+// console.log(n2);
+// for (let i = 3; i <= 10; i++) {
+//     let next = n1 + n2;
+//     console.log(next);
+//     n1 = n2;
+//     n2 = next;
+// }
 
-// 8.	Reverse counting 10–1.
-for (let d = 10; d >= 1; d--) {
-   console.log(d);
-}
+// // 8.	Reverse counting 10–1.
+// for (let d = 10; d >= 1; d--) {
+//    console.log(d);
+// }
 
-// 9.	Print pattern of * (triangle).
- for (let i = 1; i <= 5; i++) {
-    row = "";
-    for (let star = 1; star <= i; star++) {
-        row = row + "#";
-    }
-    console.log(row)
- }
+// // 9.	count pattern of * (triangle).
+//  for (let i = 1; i <= 5; i++) {
+//     row = "";
+//     for (let star = 1; star <= i; star++) {
+//         row = row + "#";
+//     }
+//     console.log(row)
+//  }
 
-// revert this triangle example :
-for (let i = 1; i <= 5; i++) {
-  row = "";
-  for (let j = 1; j <= 5 - i + 1; j++) {
-    row = row + "#"
-  }
-  console.log(row)
-}
+// // revert this triangle example :
+// for (let i = 1; i <= 5; i++) {
+//   row = "";
+//   for (let j = 1; j <= 5 - i + 1; j++) {
+//     row = row + "#"
+//   }
+//   console.log(row)
+// }
 
-// 10.	While loop: 1–10.
-let n = 1
-while (n <= 10) {
-  console.log(n++);
-}
+// // 10.	While loop: 1–10.
+// let n = 1
+// while (n <= 10) {
+//   console.log(n++);
+// }
 
-// 11.	While loop: sum of 1–50.
-let sum1 = 0;
-let n3 = 1;
-while(n3 <= 50){
-  sum1 = sum1 + n3;
-  n3++;
-}
-console.log(sum1)
+// // 11.	While loop: sum of 1–50.
+// let sum1 = 0;
+// let n3 = 1;
+// while(n3 <= 50){
+//   sum1 = sum1 + n3;
+//   n3++;
+// }
+// console.log(sum1)
 
 // 12.	While loop: reverse digits of number.
 
-// 13.	Do while loop: print 1–5.
+// 13.	Do while loop: count 1–5.
+// let count = 1;
+// do {
+//   console.log(count);
+//   count++;
+// } while (count < 5);
+
 // 14.	Do while: input until user enters 0.
+
 // 15.	Nested loops: multiplication table 1–5.
 // 16.	For…of: iterate array.
 // 17.	For…of: iterate string.
@@ -103,4 +110,4 @@ console.log(sum1)
 // 27.	Nested loop for 2D array.
 // 28.	Loop to check prime numbers 1–50.
 // 29.	Loop with flag variable.
-// 30.	Loop to print sum of even numbers.
+// 30.	Loop to count sum of even numbers.
